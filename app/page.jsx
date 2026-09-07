bafc7b7d8160b2e614042802e42dcdb4e7cf2aa4
m46mm <myhrm46@gmail.com>
2026-09-07 12:30:53 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@
 import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://vriifilccnczqiuthqiw.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyaWlmaWxjY25jenFpdXRocWl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2NzUxNTUsImV4cCI6MjEwNDI1MTE1NX0.bGmSpCWXXhymvHb3psDA80P80cngVuPjXx3uuYLEPWA'
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default function Home() {
